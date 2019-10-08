f6636c0fa1fcfb4f11333d406dafeb04b3abb9ab
Huide Zhou <prettyage.new@gmail.com>
2019-10-09 05:45:03 +0800

    add comment in user and user_event tables

--- database/user.sql
+++ database/user.sql
@@ -1,9 +1,10 @@
+/** user table */
 CREATE TABLE t_user (
-    id serial primary key,
-    uuid varchar(64) not null unique,
-    email varchar(255) not null unique,
-    name varchar(255),
-    password varchar(255) not null,
-    created_at timestamp not null,
-    available boolean not null
+    id serial primary key, /**< record ID */
+    uuid varchar(64) not null unique, /**< user UUID */
+    email varchar(255) not null unique, /**< user email */
+    name varchar(255), /**< user name */
+    password varchar(255) not null, /**< user password (encrupted) */
+    created_at timestamp not null, /**< created timestamp */
+    available boolean not null /**< availability of user */
 );
--- database/user_event.sql
+++ database/user_event.sql
@@ -1,9 +1,18 @@
+/** user event record table */
 CREATE TABLE t_user_event (
-    id serial primary key,
-    ts timestamp not null,
-    user_id varchar(64) not null,
+    id serial primary key, /**< record ID */
+    ts timestamp not null, /**< event timestamp */
+    user_id varchar(64) not null, /**< relevent user UUID */
+    /** event type: \
+        * 0 --- none/invalid \
+        * 1 --- register \
+        * 2 --- login \
+        * 3 --- logout \
+        * 4 --- modify \
+        * 5 --- destroy
+     */
     type smallint not null,
-    info text,
-    login_ip varchar(50),
-    browser varchar(255)
+    info text, /**< event information (meta-data in json) */
+    login_ip varchar(50), /**< login IP */
+    browser varchar(255) /**< used browser */
 );
